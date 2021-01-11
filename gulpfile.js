1229f472783b40bc1dd18e037dc653e7f11556e5
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-01-11 16:40:48 -0500

    Bundle js modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const {series} = require('gulp');
 const browserSync = require('browser-sync').create();
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
+const sourcemaps = require('gulp-sourcemaps');
 
 function watch (){
   browserSync.init({
@@ -8,4 +12,15 @@ function watch (){
   });
 }
 
-exports.default = series(watch);
+function scripts() {
+  return gulp.src('js/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(webpackStream(webpackConfig), webpack)
+      // .pipe(babel())
+      // IMPORTANT: Make sure the references tp js files point at all.js
+      // .pipe(concat('all.js'))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('dist/js'));
+}
+
+exports.default = series(scripts ,watch);
