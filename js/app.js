4f75805f5fdecfa9a5d0274fd9c98b158bb0cb41
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-01-28 15:41:52 -0500

    Add buttons functionality

--- js/app.js
+++ js/app.js
@@ -6,10 +6,21 @@ const toDoContainer =document.getElementById('to-do-items-container');
 const addBtn = document.getElementById('add-to-do');
 const parallax = document.getElementsByClassName('img-div');
 const spread = [...parallax];
+const prioritiesContainer = document.getElementById('priorities-box');
+const prioritiesList = document.getElementById('priorities-list');
 
 // Add to-do
 addBtn.addEventListener('click', function() {
-  new lib.Item(inputField.value, toDoContainer);
+  new lib.Item(inputField.value, toDoContainer, prioritiesList, inputField);
+});
+
+
+//  Config enter key's event listener
+inputField.addEventListener('keyup', function(e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    addBtn.click();
+  }
 });
 
 
--- js/functions.js
+++ js/functions.js
@@ -1,43 +1,90 @@
 // Add to-do functionality
 export class Item {
-  constructor(itemName, boxConatainer) {
-    this.createDiv(itemName, boxConatainer);
+  constructor(itemName, boxContainer, prioritiesList, inputField) {
+    this.createDiv(itemName, boxContainer,
+        prioritiesList, inputField);
   }
 
 
-  createDiv(itemName, boxConatainer) {
-    const input = document.createElement('input');
-    input.value = itemName;
-    input.disable = true;
-    input.classList.add('item-input');
-    input.type = 'text';
-
-
+  createDiv(itemName, boxContainer, prioritiesList, inputField) {
     const itemBox =document.createElement('div');
     itemBox.classList.add('item-box');
+
+
+    const input = document.createElement('input');
+    input.value = itemName;
+    // console.log(prioritiesContainer);
+    input.classList.add('item-input');
+    input.disabled = true;
+    input.type = 'text';
 
 
     const editBtn = document.createElement('button');
     editBtn.classList.add('editBtn');
     editBtn.textContent = '+';
+    editBtn.addEventListener('click', ()=> this.edit(input, editBtn));
 
 
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('removeBtn');
     removeBtn.textContent = '-';
+    removeBtn.addEventListener('click',
+        ()=>this.remove(boxContainer, itemBox));
 
 
     const priorityBtn = document.createElement('button');
+    const orderedList = document.createElement('ol');
     priorityBtn.classList.add('priorityBtn');
     priorityBtn.textContent = 'P';
+    priorityBtn.addEventListener('click', ()=>{
+      this.addPriority(itemName, prioritiesList);
+    });
 
 
-    boxConatainer.appendChild(itemBox);
+    boxContainer.appendChild(itemBox);
 
     itemBox.appendChild(input);
     itemBox.appendChild(editBtn);
     itemBox.appendChild(removeBtn);
     itemBox.appendChild(priorityBtn);
+    inputField.value = '';
+  }
+
+
+  //  Edit method
+  edit(input, btn) {
+    input.disabled = !input.disabled;
+    if (input.disabled != true) {
+      btn.style.backgroundColor = 'lime';
+    } else {
+      btn.style.backgroundColor = '#B1DBEC';
+    }
+  }
+
+
+  // Remove method
+  remove(container, itemBox) {
+    container.removeChild(itemBox);
+  }
+
+
+  // Priority method
+
+  addPriority(value, list) {
+    const listItem = document.createElement('li');
+    listItem.appendChild(document.createTextNode(value));
+    list.appendChild(listItem);
+    listItem.classList.add('priorities-list-item');
+    listItem.addEventListener('click', ()=>{
+      listItem.classList.add('done');
+    });
+    listItem.addEventListener('dblclick', ()=>{
+      list.removeChild(listItem);
+    });
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = '-';
+    removeBtn.classList.add('removeBtn');
   }
 }
 
